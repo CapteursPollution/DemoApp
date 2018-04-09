b6cddede6f1177ac8288452743013c679fd194e7
nrutemby <nil.rutembya@sogeti.com>
2018-04-09 15:35:34 +0200

    Methods onOptionsSelectedItem & onCreateOptionMenu overridden

--- app/src/main/java/com/example/android/demoapp/MainActivity.java
+++ app/src/main/java/com/example/android/demoapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.demoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = new MenuInflater(this);
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (R.id.action_refresh == item.getItemId()) {
+            // refresh main activity
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
