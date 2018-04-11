a80aa2295e92228ac0be3f7176ec4d84fe29dfda
nrutemby <nil.rutembya@sogeti.com>
2018-04-11 17:47:04 +0200

    Add methods to display graph using MPAndroidChart:v3.0.2 framework

--- app/src/main/java/com/example/android/demoapp/MainActivityBis.java
+++ app/src/main/java/com/example/android/demoapp/MainActivityBis.java
@@ -17,6 +17,7 @@
  */
 package com.example.android.demoapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
@@ -26,9 +27,20 @@ import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 
 import com.example.android.demoapp.network.NetworkUtils;
@@ -48,17 +60,21 @@ public class MainActivityBis extends AppCompatActivity implements
     private TextView mSearchResultsTextView;
     private TextView mErrorMessageDisplay;
     private ProgressBar mLoadingIndicator;
+    private LineChart mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_bis);
 
-        mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
-        mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
-        mSearchResultsTextView = (TextView) findViewById(R.id.tv_search_results_json);
-        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
-        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+        setContentView(R.layout.activity_main_bis);
+        mSearchBoxEditText = findViewById(R.id.et_search_box);
+        mUrlDisplayTextView = findViewById(R.id.tv_url_display);
+        mSearchResultsTextView = findViewById(R.id.tv_search_results_json);
+        mErrorMessageDisplay = findViewById(R.id.tv_error_message_display);
+        mLoadingIndicator = findViewById(R.id.pb_loading_indicator);
+        mChart = findViewById(R.id.lineChart);
+
+        initChart(mChart);
 
         if (savedInstanceState != null) {
             String SearchUrlString = savedInstanceState.getString(SEARCH_URL_EXTRA);
@@ -66,6 +82,131 @@ public class MainActivityBis extends AppCompatActivity implements
         }
 
         getSupportLoaderManager().initLoader(SEARCH_LOADER_ID, null, this);
+    }
+
+    private void initChart(LineChart mChart) {
+
+        // customize line chart
+        Description des = new Description();
+        des.setText("");
+        mChart.setDescription(des);
+
+        // enable value highlighting
+        // mChart.setDefaultFocusHighlightEnabled(true);
+
+        // enable touch gestures
+        mChart.setTouchEnabled(true);
+
+        // enable scaling & dragging
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+
+        // enable pinch zoom to avoid scaling x and y axis separately
+        mChart.setPinchZoom(true);
+
+        // alternative background color
+        mChart.setBackgroundColor(Color.WHITE);
+
+        // DATA
+        LineData data = new LineData();
+        data.setValueTextColor(Color.RED);
+
+        // add data to line chart
+        mChart.setData(data);
+
+        // get legend object
+        Legend l = mChart.getLegend();
+
+        // customize legend
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.BLACK);
+
+
+        mChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        XAxis x1 = mChart.getXAxis();
+        x1.setTextColor(Color.BLACK);
+        x1.setDrawGridLines(true);
+        x1.setAvoidFirstLastClipping(true);
+        x1.setEnabled(false);
+
+        YAxis y1 = mChart.getAxisLeft();
+        y1.setTextColor(Color.BLACK);
+        // auto set y axis min max with 5% spacing:
+        float percent = 5;
+        y1.setSpaceTop(percent);
+        y1.setSpaceBottom(percent);
+        y1.setDrawGridLines(true);
+
+        YAxis y12 = mChart.getAxisRight();
+        y12.setEnabled(false);
+
+    }
+
+    private void addEntry(Float a, Float b) {
+        LineData data = mChart.getData();
+
+        if (data != null) {
+
+            // APhsA
+            ILineDataSet set = data.getDataSetByIndex(0);
+
+            if (set == null) {
+                set = createSet("APhsA");
+                data.addDataSet(set);
+            }
+
+            data.addEntry(new Entry(set.getEntryCount(), a), 0);
+
+            // APhsB
+            set = data.getDataSetByIndex(1);
+
+            if (set == null) {
+                set = createSet("APhsB");
+                data.addDataSet(set);
+            }
+
+            data.addEntry(new Entry(set.getEntryCount(), b), 1);
+
+            // let the chart know it's data has changed
+            data.notifyDataChanged();
+            mChart.notifyDataSetChanged();
+
+            // limit the number of visible entries
+//            mChart.setVisibleXRangeMaximum(15);
+            float minXRange = 10;
+            float maxXRange = 10;
+            mChart.setVisibleXRange(minXRange, maxXRange);
+
+            // move to the latest entry
+            mChart.moveViewToX(data.getEntryCount());
+        }
+    }
+
+    private LineDataSet createSet(String label) {
+        LineDataSet set = new LineDataSet(null, label);
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        Integer color;
+        switch (label){
+            case "APhsA":
+                color = Color.RED;
+                break;
+            case "APhsB":
+                color = Color.GREEN;
+                break;
+            default:
+                color = Color.BLACK;
+        }
+        set.setColors(color);
+        set.setCircleColor(color);
+        set.setLineWidth(2f);
+        set.setCircleRadius(4f);
+        set.setValueTextColor(color);
+        set.setValueTextSize(10f);
+        // To show values of each point
+        set.setDrawValues(true);
+
+        return set;
     }
 
     private void makeSearchQuery() {
@@ -128,6 +269,7 @@ public class MainActivityBis extends AppCompatActivity implements
             }
         };
     }
+
 
     @Override
     public void onLoadFinished(Loader<String> loader, String data) {
